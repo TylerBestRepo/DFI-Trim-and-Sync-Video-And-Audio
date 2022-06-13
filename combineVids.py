f830165cc41c61114eefb952f3045dc04dcde4ad
Tyler Best <tyler.best.235@gmail.com>
2022-06-13 20:00:34 +1000

    Combines vids with the same file type very quickly now.

--- combineVids.py
+++ combineVids.py
@@ -1,9 +1,55 @@
 from moviepy.editor import *
+from tkinter import Tk
+from tkinter import messagebox
+from tkinter.filedialog import askopenfilename
+import csv
 
-clip_1 = VideoFileClip('1.MOV')
-clip_2 = VideoFileClip('2.MOV')
+from numpy import number
+
+files_list = []
+numberOfClips = input("How many files are you combining here?: ")
+temp_file = []
+print(numberOfClips)
+numberOfClips = int(numberOfClips)
+
+looper = 0
+
+while looper < int(numberOfClips):
+    Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+    messagebox.showinfo("Ahoy", "Select a video file in the correct order")
+    temp_file = askopenfilename()
+    files_list.append("file '" + temp_file + "'")
+
+    looper += 1
+
+print(files_list)
+
+with open ('concatenationTXT.txt', 'w') as f:
+    writer = csv.writer(f)
+    i = 0
+    for item in files_list:
+        writer.writerow([item])
+    
+shell_command = "ffmpeg -f concat -safe 0 -i concatenationTXT.txt -c copy betterOuputName.MP4"
+
+if (files_list != []):
+    os.system(shell_command)
 
 
-result_clip = concatenate_videoclips([clip_1, clip_2])
+# This works but does it very slowly due to re-encoding
 
-result_clip.write_videofile('outputFile.mp4')
+# if (numberOfClips == 1):
+#     clip_1 = VideoFileClip(files_list[0])
+#     print("Its only one clip so you dont need to combine anything.")
+# elif (numberOfClips == 2):
+#     clip_1 = VideoFileClip(files_list[0])
+#     clip_2 = VideoFileClip(files_list[1])
+#     result_clip = concatenate_videoclips([clip_1, clip_2])
+# elif (numberOfClips == 3):
+#     clip_1 = VideoFileClip(files_list[0])
+#     clip_2 = VideoFileClip(files_list[1])
+#     clip_3 = VideoFileClip(files_list[2])
+#     result_clip = concatenate_videoclips([clip_1, clip_2, clip_3])
+
+
+# result_clip.write_videofile('combined2Files.mp4')
